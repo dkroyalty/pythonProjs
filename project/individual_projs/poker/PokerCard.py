7ef595b32e0cf59ee151cc6d830d6840edaf514c
康赞 <zan.kang@gumichina.com>
2015-05-29 16:59:27 +0800

    add poker project folder

---
+++ project/individual_projs/poker/PokerCard.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import sys
+import random
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class PokerCardPool():
+
+    def __init__(self, isspecial=False):
+        self.cardpool = []
+        if isspecial == False:
+            self.cardpool.append(1000000)
+            self.cardpool.append(1000001)
+        for color in range(2, 6):
+            for pt in range(len(PokerCard._CardPointList)):
+                self.cardpool.append(10**color+pt)
+
+    def dealCardsFromPool(self, times=1):
+        deallist = []
+        i = 0
+        while i < times:
+            selcard = random.randint(0, len(self.cardpool)-1)
+            deallist.append(self.cardpool[selcard])
+            self.cardpool.remove(self.cardpool[selcard])
+            i += 1
+        return deallist
+
+    def displayPoolInfo(self):
+        for eachcard in self.cardpool:
+            print PokerCard(eachcard).getCardInfo()
+
+class PokerCard():
+
+    _CardBiasDict = {
+        "Diamond":100,
+        "Club":   1000,
+        "Heart":  10000,
+        "Spade":  100000,
+        "Joker":  1000000,
+    }
+    _CardPointList = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+    def __init__(self, cardvalue):
+        assert self.checkValueLegal(cardvalue)
+        self.cardvalue = cardvalue
+        self.status = 0
+
+    def checkIsJoker(self, value):
+        val = value - self._CardBiasDict["Joker"]
+        if val == 0 or val == 1:
+            return True
+        return False
+
+    def checkValueLegal(self, cardvalue):
+        cardtype = self.getCardType(cardvalue)
+        assert cardtype is not None
+        val = self.getCardValue(cardvalue)
+        if self.checkIsJoker(cardvalue):
+            return True
+        elif val not in range(len(self._CardPointList)):
+            return False
+        return True
+
+    def getCardValue(self, value):
+        for eachkey in self._CardBiasDict.keys():
+            delta = value - self._CardBiasDict[eachkey]
+            if delta >= 0 and delta < 20:
+                return value % self._CardBiasDict[eachkey]
+        return None
+
+    def getCardType(self, value):
+        for eachkey in self._CardBiasDict.keys():
+            delta = value - self._CardBiasDict[eachkey]
+            if delta >= 0 and delta < 20:
+                return eachkey
+        return None
+
+    def getCardInfo(self):
+        infostr = ""
+        fullname = self.getCardType(self.cardvalue)
+        if self.checkIsJoker(self.cardvalue):
+            infostr = "%s-%d" % (fullname, self.getCardValue(self.cardvalue))
+        else:
+            infostr = "%s-%s" % (fullname,
+                self._CardPointList[self.getCardValue(self.cardvalue)])
+        return infostr
+
+if __name__ == "__main__":
+    testpool = PokerCardPool()
+    #print testpool.dealCardsFromPool(5)
+    testpool.displayPoolInfo()
+    #testsppool = PokerCardPool(True)
